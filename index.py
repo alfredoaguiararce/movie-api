86ae0f698a2191c4e6dbf4044e65833203861ecd
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-09 23:11:09 -0700

    Update index.py

--- index.py
+++ index.py
@@ -12,7 +12,23 @@ movies = [
         'year': '2009',
         'rating': 7.8,
         'category': 'Acción'    
-    } 
+    },
+    {
+        'id': 2,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    },
+    {
+        'id': 3,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    },
 ]
 
 
@@ -27,3 +43,11 @@ def message_html():
 @app.get('/movies', tags=['movies'])
 def get_movies():
     return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie_by_id(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    
+    return None
