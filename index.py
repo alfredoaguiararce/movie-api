87f4946e298e2ac6facd19c3db3d38af059c7d9a
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-09 23:04:05 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 app.title = "FastApi app"
 app.version = "0.0.1"
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    } 
+]
+
 
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world!"
+    return {"hello": "world",}
+
+@app.get('/html', tags=['home'])
+def message_html():
+    return HTMLResponse('<h1>Hello World</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
