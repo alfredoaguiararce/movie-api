74e9a9aba3e80363479d1ffefb04590612fd515f
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-09 22:56:28 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "FastApi app"
+app.version = "0.0.1"
 
-@app.get('/')
+
+@app.get('/', tags=['home'])
 def message():
     return "Hello world!"
